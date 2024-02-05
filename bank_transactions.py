8f2cb8238d1ad2750c962199ff78a56b784ab633
tlavallee4 <tlavallee4@academic.rrc.ca>
2024-02-05 08:58:14 -0600

    Updating bank_transactions file

--- bank_transactions.py
+++ bank_transactions.py
@@ -6,15 +6,15 @@ Date: 02/01/2024
 
 """
 
+# Define interface
+
 import random 
 D = "Deposit"
 W = "Withdraw"
 Q = "Quit"
 
 balance = random.randint(-1000,10000)
-balance = f"${balance:,.2f}"
-string_balance = str(balance)
-
+# balance = f"${balance:,.2f}"
 
 ## print(f"${random.randint(-1000,10000):,.2f}")
 ## convert to dollar by 2 decimal place 
@@ -27,7 +27,7 @@ line_1 = 'PIXELL RIVER FINANCIAL'
 centered_line_1 = line_1.center(width, fill_space)
 line_2 = 'ATM Simulator'
 centered_line_2 = line_2.center(width, fill_space)
-line_3 = 'Your current balance is: ' + string_balance
+line_3 = 'Your current balance is: ' + f"${balance:,.2f}"
 centered_line_3 = line_3.center(width, fill_space)
 line_4 = 'Deposit: D'
 centered_line_4 = line_4.center(width, fill_space)
@@ -46,5 +46,47 @@ print(line + "\n"
     + centered_line_6 + "\n"
     + line
     )
-    
+
+# Incorporate Transactions 
+selection = input("Enter your selection: ")
+capitalized_selection = selection.capitalize()
+
+
+invalid_line = 'INVALID SELECTION'
+insufficient_line = 'INSUFFICIENT FUNDS'
+centered_invalid_line = invalid_line.center(width, fill_space)
+centered_insufficient_line = insufficient_line.center(width, fill_space)
+
+if capitalized_selection == 'D':
+    transaction = float(input("Enter your amount of transaction: "))
+    current_balance = transaction + balance
+    line_7 =  'Your current balance is: ' + f"${current_balance:,.2f}"
+    centered_line_7 = line_7.center(width, fill_space)
+    print(line + "\n" 
+        + centered_line_7 +"\n"
+        + line)
+elif capitalized_selection == 'W':
+    transaction = float(input("Enter your amount of transaction: "))
+    if transaction > balance : 
+        print(line + "\n" 
+        + centered_insufficient_line +"\n"
+        + line)
+    elif transaction <= balance :
+        current_balance = balance - transaction
+        line_7 =  'Your current balance is: ' + f"${current_balance:,.2f}"
+        centered_line_7 = line_7.center(width, fill_space)
+        print(line + "\n" 
+        + centered_line_7 +"\n"
+        + line)
+elif capitalized_selection == "Q":
+    centered_quit_line = Q.center(width, fill_space)
+    print(line + "\n"
+          + centered_quit_line + "\n"
+          + line)
+else: 
+    print(line + "\n"
+    + centered_invalid_line + "\n"
+    + line
+    )
+
     
